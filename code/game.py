2b0209ef252d432a485e4bf54180c297128e82c6
DeividSouza2207 <deividgreenday90@gmail.com>
2025-10-06 21:30:04 -0300

    Creating fonts menu

--- code/game.py
+++ code/game.py
@@ -1,20 +1,18 @@
 import pygame
 
+from code.Const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(700, 580))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
+
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
 
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         pygame.quit()  # fecha a janela
-            #         quit()  # encerra o pygame
